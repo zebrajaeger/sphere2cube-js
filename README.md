0420960ae7b6c2c858ce4bf5edbb11d319d27264
Lars Brandt <brandt@silpion.de>
2020-07-22 13:04:30 +0200

    improve cli

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 Converts  
 - full spheric panorama image to viewer (equirectangular)
 - 360° panorama image to viewer (y to small for equirectangular)
-- partial panorama image ti viewer (TODO)
+- partial panorama image to viewer (TODO)
 
 Reads 
 - PSD and PSB with RAW or RLE Encoding
@@ -20,6 +20,52 @@ Writes
 - html (pannellum implementation)
 - all above as zip file 
 
-Many Thanks to
+
+## Installation (TODO make release for npmjs)
+
+```bash
+$ npm install -g  @zebrajaeger/createpano
+```
+
+## Usage
+
+### Minimum
+
+```bash
+$ createpano -i sourceimage.psd
+```
+
+## Options
+```bash
+Usage: cli [options]
+
+Options:
+  -V, --version                    output the version number
+  -i, --input <path>               Input image (mandatory)
+  -pa, --panoAngle <degree>        Angle of pano (default: "360")
+  -py, --panoYOffset <degree>      Y-Offset in degree [-90.0...90.0] (default: "0")
+  -o, --output <path>              Output folder (default: ".")
+  -te, --targetSize <pixel>        Image edge length of a face @ max resolution (default: inputImage.x / 4)
+  -ti, --tilesIgnore               Dont render tiles
+  -ts, --tileSize <pixel>          Tile size (default: "512")
+  -tq, --tileQuality <percent>     Jpg Image quality of tiles in percent (default: "85")
+  -pi, --previewIgnore             Dont render preview
+  -pp, --previewPath <path>        path and name of preiew image (default: "./preview.png")
+  -pw, --previewWidth <pixel>      Preview width (default: "1000")
+  -pw, --previewQuality <percent>  Preview quality in percent (default: "85")
+  -hi, --htmlIgnore                Don't render html
+  -ht, --htmlTitle <name>          Head-Title-Tag (default: inputImage)
+  -zi, --zipIgnore                 Don't zip
+  -zp, --zipPath <path>            Path for Zip File (default: "pano.zip")
+  -v, --verbose                    verbose
+  -h, --help                       display help for command
+
+
+```
+
+
+
+## Many Thanks to
+
 - https://stackoverflow.com/questions/29678510/convert-21-equirectangular-panorama-to-cube-map
 - https://stackoverflow.com/questions/1726630/formatting-a-number-with-exactly-two-decimals-in-javascript
--- src/cli.js
+++ src/cli.js
@@ -2,19 +2,27 @@
 
 const converter = require('./converter');
 const {program} = require('commander');
+const fs = require('fs');
+const path = require('path');
 
-program.version('0.0.1');
+// find and parse package.json
+const p = process.argv[1]
+const pjp = path.resolve(p, '../..', 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(pjp, 'utf-8'))
+
+// cli
+program.version(packageJson.version)
 program
-    .requiredOption('-i, --input <path>', 'Input image')
-    .option('-a, --angle <degree>', 'Angle of pano', '360')
-    .option('-y, --yOffset <degree>', 'Y-Offset in degree [-90.0...90.0]', '0')
+    .requiredOption('-i, --input <path>', 'Input image (mandatory)')
+    .option('-pa, --panoAngle <degree>', 'Angle of pano', '360')
+    .option('-py, --panoYOffset <degree>', 'Y-Offset in degree [-90.0...90.0]', '0')
 
     .option('-o, --output <path>', 'Output folder', '.')
-    .option('-s, --targetSize <pixel>', 'Image edge length of a face @ max resolution', undefined)
+    .option('-te, --targetSize <pixel>', 'Image edge length of a face @ max resolution (default: inputImage.x / 4)', undefined)
 
     .option('-ti, --tilesIgnore', 'Dont render tiles', 'false')
     .option('-ts, --tileSize <pixel>', 'Tile size', '512')
-    .option('-tq, --tileQuality <percent>', 'Image quality of tiles in percent', '85')
+    .option('-tq, --tileQuality <percent>', 'Jpg Image quality of tiles in percent', '85')
 
     .option('-pi, --previewIgnore', 'Dont render preview', 'false')
     .option('-pp, --previewPath <path>', 'path and name of preiew image', './preview.png')
@@ -22,10 +30,10 @@ program
     .option('-pw, --previewQuality <percent>', 'Preview quality in percent', '85')
 
     .option('-hi, --htmlIgnore', 'Don\'t render html', 'false')
-    .option('-ht, --htmlTitle <name>', 'Head-Title-Tag', '')
+    .option('-ht, --htmlTitle <name>', 'Head-Title-Tag (default: inputImage)', undefined)
 
-    .option('-zp, --zipPath <path>', 'Path for Zip File', 'pano.zip')
     .option('-zi, --zipIgnore', 'Don\'t zip', 'false')
+    .option('-zp, --zipPath <path>', 'Path for Zip File', 'pano.zip')
 
     .option('-v, --verbose', 'verbose', 'false')
 
@@ -36,7 +44,6 @@ let title = program.input;
 if (program.htmlTitle && program.htmlTitle.length !== 0) {
     title = program.htmlTitle;
 }
-
 
 const cfg = {
     targetImgSize: program.targetSize,
